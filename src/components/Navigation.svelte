b41a1fee662794f1336ad4b54862e9c679cbb29b
Lennart de Knikker <lennart.deknikker@wearetriple.com>
2024-07-11 23:01:25 +0200

    add basic app layout

---
+++ src/components/Navigation.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	export let menuItems: {
+		id: string;
+		label: string;
+		href: string;
+	}[] = [];
+</script>
+
+<nav>
+	<a href="/">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" xml:space="preserve"
+			><path
+				d="M27 9h-1c0-1.65-1.35-3-3-3h-1c-1.65 0-3 1.35-3 3v4h-6V9c0-1.65-1.35-3-3-3H9C7.35 6 6 7.35 6 9H5c-1.65 0-3 1.35-3 3v8c0 1.65 1.35 3 3 3h1c0 1.65 1.35 3 3 3h1c1.65 0 3-1.35 3-3v-4h6v4c0 1.65 1.35 3 3 3h1c1.65 0 3-1.35 3-3h1c1.65 0 3-1.35 3-3v-8c0-1.65-1.35-3-3-3zM5 21c-.55 0-1-.45-1-1v-8c0-.55.45-1 1-1h1v10H5zm6 2c0 .55-.45 1-1 1H9c-.55 0-1-.45-1-1V9c0-.55.45-1 1-1h1c.55 0 1 .45 1 1v14zm2-6v-2h6v2h-6zm11 6c0 .55-.45 1-1 1h-1c-.55 0-1-.45-1-1V9c0-.55.45-1 1-1h1c.55 0 1 .45 1 1v14zm4-3c0 .55-.45 1-1 1h-1V11h1c.55 0 1 .45 1 1v8z"
+			/></svg
+		>
+	</a>
+  <h1>Workout app</h1>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+    align-items: center;
+    gap: 1rem;
+	}
+
+  h1 {
+    margin: 0;
+  }
+  
+  a {
+    height: 3rem;
+		font-size: 3rem;
+  }
+
+	ul {
+		list-style-type: none;
+		padding: 0;
+		display: flex;
+	}
+
+	svg {
+		width: 1em;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Navigation from '../components/Navigation.svelte';
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<header>
+  <Navigation />
+</header>
+<main>
+	<slot />
+</main>
+
+<style>
+	:global(html) {
+    font-family: "Rubik", sans-serif;
+		font-size: 10px;
+	}
+
+  :global(h1) {
+    font-size: 2rem;
+  }
+</style>
