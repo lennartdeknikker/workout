a5ac9857d9b854195c0b060903f57db47b1ec02c
Lennart de Knikker <lennartdeknikker@gmail.com>
2024-09-13 13:16:47 +0200

    add pdfs

--- src/lib/firebase.client.ts
+++ src/lib/firebase.client.ts
@@ -11,10 +11,13 @@ export let auth: Auth;
 
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
 	useEmulator: import.meta.env.VITE_FIREBASE_USE_EMULATOR === 'true',
-	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN
-};
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  };
 
 export const initializeFirebase = () => {
 	if (!browser) {
